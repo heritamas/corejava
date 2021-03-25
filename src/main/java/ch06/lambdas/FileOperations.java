3623d7a9125083a1f3882578e3aec5401da6e626
Csenge Maruzsi <csenge.maruzsi@cloudera.com>
2021-03-25 12:17:01 +0100

    Pipe and Pipe2

--- src/main/java/ch06/lambdas/FileOperations.java
+++ src/main/java/ch06/lambdas/FileOperations.java
@@ -6,8 +6,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collection;
-import java.util.Set;
+import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.stream.Stream;
 
 class Driver {
@@ -24,11 +25,31 @@ class Driver {
         Consumer<String> groupConsumer = new Consumer<String>() {
             @Override
             public void accept(String s) {
-                actions.stream().forEach(stringConsumer -> stringConsumer.accept(s));
+               actions.stream().forEach(action -> action.accept(s));
             }
         } ;
 
         Files.lines(path).forEach(groupConsumer);
+    }
+    
+    Stream<String> pipe(Collection<Function<String, String>> actions) throws IOException {
+        Function<String, String> groupFunction = new Function<String, String>(){
+
+            @Override
+            public String apply(String s) {
+                String result = s;
+                for(Function<String, String> action : actions){
+                    result = action.apply(result);
+                }
+                return result;
+            }
+        };
+        return Files.lines(path).map(groupFunction);
+    }
+
+    Stream<String> pipe2(Collection<Function<String, String>> actions) throws IOException {
+        Function<String, String> groupFunction = actions.stream().reduce(x -> x, (f1,f2) -> f2.compose(f1));
+        return Files.lines(path).map(groupFunction);
     }
 }
 
@@ -38,6 +59,11 @@ public class FileOperations {
     public static void main(String[] args) throws URISyntaxException, IOException {
         Driver drv = new Driver("lambdafile.txt");
 
-        drv.process(Set.of(System.out::println));
+//        drv.process(List.of(System.out::println, str -> System.out.println(str.toUpperCase()), str -> System.out.println(str.contains("ac"))));
+        drv.pipe2(List.of(str -> str.replaceAll("N", "79"), 
+                str -> str.toUpperCase(), 
+                str -> str.replaceAll("AC", "sddshgdrfghsths")))
+                .forEach(System.out::println);
+        
     }
 }
--- src/main/java/ch06/lambdas/FizzBuzz.java
+++ src/main/java/ch06/lambdas/FizzBuzz.java
@@ -6,6 +6,23 @@ import java.util.stream.IntStream;
 public class FizzBuzz {
     public static void main(String[] args) {
         IntStream.range(1, 100)
-                .forEach(System.out::println);
+                .forEach(x -> {
+                    String buffer = "";
+                    if (divider(x, 3)){
+                        buffer += "Fizz";
+                    }
+                    if (divider(x, 5)){
+                        buffer += "Buzz";
+                    }
+                    if (buffer.isEmpty()){
+                        buffer += x;
+                    }
+                    System.out.println(buffer);
+                    
+                });
+    }
+    
+    private static boolean divider(Integer number, int divider) {
+        return number % divider == 0;
     }
 }
