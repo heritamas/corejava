bbeeebd676cab5eb490eb4240f803d2463c82f8d
Csenge Maruzsi <csenge.maruzsi@cloudera.com>
2021-09-10 15:12:15 +0200

    p2ch1 before stream reductions

--- src/main/java/p2ch01/StreamTasks.java
+++ src/main/java/p2ch01/StreamTasks.java
@@ -4,6 +4,10 @@ import ch09.TextReader;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.PrimitiveIterator;
+import java.util.function.IntSupplier;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class StreamTasks {
@@ -11,23 +15,24 @@ public class StreamTasks {
     // get stream of lines from file
     private static Stream<String> getLineStream() throws URISyntaxException, IOException {
         TextReader tr = new TextReader("Hamlet.txt");
-        return ???;
+        return tr.getContent().stream();
     }
 
     // get stream of words from file
     private static Stream<String> getWordStream() throws URISyntaxException, IOException {
         TextReader tr = new TextReader("Hamlet.txt");
-        return ???;
+        return tr.getContent().stream()
+            .flatMap(line -> Arrays.stream(line.split("\\PL+")));
     }
 
     // get lines containing a certain substring
     private static Stream<String> filterLines(Stream<String> lineStream, String has) {
-        return ???;
+        return lineStream.filter(line -> line.contains(has));
     }
 
     // first n elements of stream
     private static Stream<String> firstNElements(Stream<String> stream, long n) {
-        return ???;
+        return stream.limit(n);
     }
 
     // Collatz:
@@ -36,29 +41,41 @@ public class StreamTasks {
     // 3n+1 if n was odd
     // stop at reaching 1
     private static Stream<Long> generateCollatzSequence(Long start) {
-        return ???;
-
+        return Stream.iterate(start, num -> num != 1L, num -> {
+            if (num % 2 == 0) {
+                return num/2;
+            }
+            else {
+                return 1+(3*num);
+            }
+        });
     }
 
     // prefix each element with its order number
     private static Stream<String> numberElements(Stream<String> stream) {
-        return ???;
+        IntStream intStream = IntStream.iterate(1, num -> num + 1);
+        PrimitiveIterator.OfInt iterator = intStream.iterator();
+        return stream.map(string -> String.format("%d: %s", iterator.nextInt(), string));
 
     }
 
     // list words until someone is mentioned
     private static Stream<String> untilMentioned(Stream<String> origin, String who) {
-        return ???;
+        return origin.takeWhile(word -> !word.equals(who));
     }
 
     // list roles
     private static Stream<String> alphabeticRoles(Stream<String> origin) {
-        return ???;
+        return origin.filter(word -> word.matches("\\p{Lu}{4,}")).sorted().distinct();
     }
 
 
     public static void main(String[] args) throws URISyntaxException, IOException {
 
-        getWordStream().forEach(System.out::println);
+//        firstNElements(getLineStream(), 12).forEach(System.out::println);
+//        generateCollatzSequence(27L).forEach(System.out::println);
+//        numberElements(getLineStream()).forEach(System.out::println);
+//        untilMentioned(getWordStream(), "HORATIO").forEach(System.out::println);
+        alphabeticRoles(getWordStream()).forEach(System.out::println);
     }
 }
