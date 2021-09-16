b75ba2340d9cbe3f5092ba66acae019823bf58d5
Csenge Maruzsi <csenge.maruzsi@cloudera.com>
2021-09-16 15:29:13 +0200

    optional, collect

--- src/main/java/p2ch01/StreamTasks2.java
+++ src/main/java/p2ch01/StreamTasks2.java
@@ -5,12 +5,17 @@ import ch09.TextReader;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.AbstractMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
+import java.util.StringJoiner;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 
@@ -31,28 +36,52 @@ public class StreamTasks2 {
 
     // infinite number sequence, helper method
     private static Stream<Long> getNumbers() {
-        return null;
+        LongStream ls = LongStream.iterate(0L, num -> num + 1L);
+        return ls.boxed();
     }
 
     // create enumerated stream from string stream
-    private static Object enumerateStream(Stream<String> input) {
-        return null;
+    private static Stream<Map.Entry<Long, String>> enumerateStream(Stream<String> input) {
+        Iterator<Long> iterator = getNumbers().iterator();
+        return input.map(word -> new AbstractMap.SimpleEntry<Long, String>(iterator.next(), word));
     }
 
     // which line has the first encounter of "whatever?"
     private static Optional<Long> getFirstEncounter(Stream<String> input, String whatever) {
-        return null;
+        Stream<Map.Entry<Long, String>> entryStream = enumerateStream(input);
+        return entryStream
+            .filter(entry -> entry.getValue().contains(whatever))
+            .map(entry -> entry.getKey())
+            .findFirst();
     }
 
     // every line where "something" is metioned
     // use optionals
-    private Stream<String> allMentions(Stream<String> input, String something) {
-        return null;
+    private static Stream<String> allMentions(Stream<String> input, String something) {
+        return input
+            .map(line -> {
+                if (line.contains(something)) {
+                    return Optional.of(line);
+                }
+                else {
+                    return Optional.<String>empty();
+                }
+            })
+            .flatMap(Optional::stream);
     }
 
-    // list of all roles
-    private List<String> allRoles(Stream<String> input) {
-        return null;
+    // list of all words 
+    // by first letter
+    private static Map<Character, Set<String>> allWords(Stream<String> input) {
+        return input
+            .filter(word -> !word.isEmpty())
+            .collect(Collectors.groupingBy(word -> word.charAt(0), Collectors.toSet()));
+    }
+    
+    private static Map<Character, Integer> getNumberOfWords(Stream<String> input) {
+        return input
+            .filter(word -> !word.isEmpty())
+            .collect(Collectors.groupingBy(word -> word.charAt(0), Collectors.collectingAndThen(Collectors.toSet(), Set::size)));
     }
 
     // ... separated by ","
@@ -74,6 +103,10 @@ public class StreamTasks2 {
 
     public static void main(String[] args) throws URISyntaxException, IOException {
 
-        getLineStream().forEach(System.out::println);
+//        getLineStream().forEach(System.out::println);
+//        enumerateStream(getLineStream()).forEach(System.out::println);
+//        System.out.println(getFirstEncounter(getLineStream(), "HAMLET").orElse(0L));
+//        allMentions(getLineStream(), "thy").forEach(System.out::println);
+        System.out.println(getNumberOfWords(getWordStream()).toString());
     }
 }
