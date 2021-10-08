57efc46161a9b911d47d18424ce102fae4420f65
Csenge Maruzsi <csenge.maruzsi@cloudera.com>
2021-10-08 09:52:38 +0200

    read files

--- src/main/java/p2ch02/InputOutput.java
+++ src/main/java/p2ch02/InputOutput.java
@@ -1,5 +1,7 @@
 package p2ch02;
 
+import ch09.TextReader;
+
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.charset.Charset;
@@ -19,23 +21,54 @@ import java.util.regex.Pattern;
 public class InputOutput {
 
     private static List<String> readReadMe() throws IOException, URISyntaxException {
-        return null;
+        Path path = Paths.get(InputOutput.class.getResource("readme.txt").toURI());
+        return Files.readAllLines(path, Charset.forName("ISO-2022-KR"));
     }
 
     private static void convertReadMe() throws URISyntaxException, IOException {
-        return;
+        List<String> original = readReadMe();
+        Path path = Paths.get(InputOutput.class.getResource(".").toURI()).resolve("converted.txt");
+        Files.write(path, original, Charset.forName("UTF-8"), StandardOpenOption.CREATE_NEW);
     }
 
     private static Map<String, Map<String, String>> readIniFile(String fileName) throws URISyntaxException, IOException {
-        return null;
+        Map<String, Map<String, String>> result = new HashMap<>();
+        Path path = Paths.get(InputOutput.class.getResource(fileName).toURI());
+        Scanner scanner = new Scanner(path, Charset.forName("UTF-8"));
+        scanner.useDelimiter("\\n");
+        while (scanner.hasNext()) {
+            String key = readHeader(scanner);
+            Map<String, String> values = readSection(scanner);
+            result.put(key, values);
+        }
+        return result;
     }
 
+
+
     private static String readHeader(Scanner in) {
-        return null;
+        String header = "";
+        while (in.hasNext("")) {
+            in.nextLine();
+        }
+        if (in.hasNext("\\[.*\\]")) {
+            header = in.next();
+        }
+        return header;
     }
 
     private static Map<String, String> readSection(Scanner in) {
-        return null;
+        Map<String, String> result = new HashMap<>();
+        while (in.hasNext(".*=.*")) {
+            Pattern delimiter = in.delimiter();
+            in.useDelimiter("=|\\n");
+            String key = in.next();
+            String value = in.next();
+            result.put(key, value);
+            in.useDelimiter(delimiter);
+            in.nextLine();
+        }
+        return result;
     }
 
     private static void writeObjects(String fileName) {
@@ -49,8 +82,9 @@ public class InputOutput {
 
 
     public static void main(String[] args) throws IOException, URISyntaxException {
-        //readReadMe().stream().forEach(System.out::println);
+//        readReadMe().stream().forEach(System.out::println);
         //convertReadMe();
         System.out.println(readIniFile("test.ini"));
+//        convertReadMe();
     }
 }
