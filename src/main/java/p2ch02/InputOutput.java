0962f0b9144b136ccd63e70026a1e42fd7ef7efc
Csenge Maruzsi <csenge.maruzsi@cloudera.com>
2021-10-13 17:04:58 +0200

    read and write objects

--- src/main/java/p2ch02/InputOutput.java
+++ src/main/java/p2ch02/InputOutput.java
@@ -2,8 +2,17 @@ package p2ch02;
 
 import ch09.TextReader;
 
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.net.URISyntaxException;
+import java.nio.ByteBuffer;
+import java.nio.DoubleBuffer;
+import java.nio.channels.FileChannel;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -15,8 +24,10 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.Scanner;
 import java.util.regex.Pattern;
+import java.util.stream.DoubleStream;
 
 public class InputOutput {
 
@@ -71,24 +82,70 @@ public class InputOutput {
         return result;
     }
 
-    private static void writeObjects(String fileName) {
-
+    private static void writeObjects(String fileName) throws IOException {
+        Map map1 = new HashMap();
+        map1.put("color", "green");
+        TestData data1 = new TestData("bogre", map1);
+        Map map2 = new HashMap();
+        map2.put("weight", "15 kg");
+        TestData data2 = new TestData("kancso", map2);
+        OutputStream outputStream = Files.newOutputStream(Path.of(fileName));
+        ObjectOutputStream oos = new ObjectOutputStream(outputStream);
+        oos.writeInt(2);
+        oos.writeObject(data1);
+        oos.writeObject(data2);
+        oos.close();
     }
 
-    private static void readObjects() {
+    private static void readObjects(String fileName) throws Exception {
+        InputStream is = Files.newInputStream(Path.of(fileName));
+        ObjectInputStream ois = new ObjectInputStream(is);
+        Integer number = ois.readInt();
+        for (int i = 0; i<number; i++) {
+            TestData o = (TestData) ois.readObject();
+            System.out.println(o);
+        }
+        ois.close();
 
     }
 
     // can a data stream read back into  a Buffer?
-    private static void readObjectsViaBuffer() {
+    private static void readObjectsViaBuffer() throws Exception {
+        String fileName = "numbers.dat";
+        OutputStream os = Files.newOutputStream(Path.of(fileName));
+        DataOutputStream dos = new DataOutputStream(os);
+        Random random = new Random();
+        DoubleStream.generate(() -> random.nextDouble())
+            .limit(10)
+            .forEach(d -> {
+                try {
+                    dos.writeDouble(d);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        dos.close();
+        
+        FileChannel fileChannel = FileChannel.open(Path.of(fileName));
+        ByteBuffer byteBuffer = ByteBuffer.allocate(100);
+        fileChannel.read(byteBuffer);
+        byteBuffer.rewind();
+        DoubleBuffer doubleBuffer = byteBuffer.asDoubleBuffer();
+        for (int i=0; i<10;i++) {
+            System.out.println(doubleBuffer.get());
+        }
+
     }
 
 
 
-    public static void main(String[] args) throws IOException, URISyntaxException {
+    public static void main(String[] args) throws Exception {
 //        readReadMe().stream().forEach(System.out::println);
         //convertReadMe();
-        System.out.println(readIniFile("test.ini"));
+//        System.out.println(readIniFile("test.ini"));
 //        convertReadMe();
+//        writeObjects("tmp.dat");
+//        readObjects("tmp.dat");
+        readObjectsViaBuffer();
     }
 }
