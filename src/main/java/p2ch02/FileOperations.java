ecb057d262fa4d8d882da9bb186e79bf46ce48f0
Csenge Maruzsi <csenge.maruzsi@cloudera.com>
2021-10-22 11:43:11 +0200

    regexes and file operations

--- src/main/java/p2ch02/FileOperations.java
+++ src/main/java/p2ch02/FileOperations.java
@@ -1,11 +1,80 @@
 package p2ch02;
 
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class FileOperations {
 
-    private static Stream<Path> listHiddenFiles(String inPath)  {
-        return null;
+    private static Stream<Path> listHiddenFiles(String inPath) throws Exception {
+            Stream<Path> entries = Files.list(Path.of(inPath));
+            return entries.filter(path -> {
+                    try {
+                        return Files.isHidden(path);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    return false;
+                })
+                ;
+    }
+
+    private static void listHiddenVoidFiles(String inPath)  {
+        try (Stream<Path> entries = Files.list(Path.of(inPath))){
+            entries.filter(path -> {
+                try {
+                    return Files.isHidden(path);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return false;
+            })
+                .forEach(System.out::println);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private static void walkFileTreeFiles(String path) throws IOException {
+        Files.walkFileTree(Path.of(path), new SimpleFileVisitor<>(){
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                if (Files.isHidden(file)) {
+                    System.out.println(file);
+                }
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
+    
+    private static List<String> findSameNamedFiles(String path) throws IOException {
+        List<String> duplicates = new ArrayList<>();
+        Files.walkFileTree(Path.of(path), new SimpleFileVisitor<>(){
+            List<String> existingFiles = new ArrayList<>();
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                if (existingFiles.contains(file.getFileName().toString())) {
+                    duplicates.add(file.getFileName().toString());
+                } 
+                existingFiles.add(file.getFileName().toString());
+                return FileVisitResult.CONTINUE;
+            }
+        });
+        return duplicates;
+    }
+    
+
+    public static void main(String[] args) throws Exception {
+//        listHiddenVoidFiles("/Users/csengeviragmaruzsi/");
+//        listHiddenFiles("/Users/csengeviragmaruzsi/")
+//            .forEach(System.out::println);
+//        walkFileTreeFiles("/Users/csengeviragmaruzsi/Documents");
+        findSameNamedFiles("/Users/csengeviragmaruzsi/Documents").forEach(System.out::println);
     }
 }
--- src/main/java/p2ch02/Regexes.java
+++ src/main/java/p2ch02/Regexes.java
@@ -1,10 +1,63 @@
 package p2ch02;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Regexes {
 
-    private List<String> split4Chars(String input) {
-        return null;
+    static class MyDate {
+        Integer year;
+        Integer month;
+        Integer day;
+        Integer hour;
+        Integer minute;
+        Integer second;
+
+        public MyDate() {
+        }
+
+        public MyDate(String year, String month, String day, String hour, String minute, String second) {
+            this.year = Integer.valueOf(year);
+            this.month = Integer.valueOf(month);
+            this.day = Integer.valueOf(day);
+            this.hour = Integer.valueOf(hour);
+            this.minute = Integer.valueOf(minute);
+            this.second = Integer.valueOf(second);
+        }
+
+        @Override
+        public String toString() {
+            return "MyDate{" +
+                "year=" + year +
+                ", month=" + month +
+                ", day=" + day +
+                ", hour=" + hour +
+                ", minute=" + minute +
+                ", second=" + second +
+                '}';
+        }
+    }
+    
+    private static MyDate parseDate(String date) {
+        Pattern pattern = Pattern.compile("(?<year>\\d{4})(.|-)(?<month>\\d{1,2})(.|-)(?<day>\\d{1,2})(.|-)\\s(?<hour>\\d{1,2}):(?<min>\\d{1,2}):(?<sec>\\d{1,2})");
+        Matcher matcher = pattern.matcher(date);
+        if (matcher.matches()) {
+            return new MyDate(matcher.group("year"), matcher.group("month"), matcher.group("day"), matcher.group("hour"), matcher.group("min"), matcher.group("sec"));
+        }
+        return new MyDate();
+    }
+    
+    private static List<String> split4Chars(String input) {
+        return List.of(input.split("(?<=\\G.{4})"));
+    }
+
+    public static void main(String[] args) {
+//        System.out.println(parseDate("2021.10.22. 11:14:33"));
+//        System.out.println(parseDate("20sfg21.10.22. 11:1ff4:33"));
+//        System.out.println(parseDate("2021.6.3. 11:2:33"));
+
+        System.out.println(split4Chars("ksdfjga adfga ASDGADFb wrgesw =p;;vpew "));
+
     }
 }
