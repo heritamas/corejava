05cd8a86e119673f7c493f275973dd15852a71fe
Tamas Heri <htamas@cloudera.com>
2021-11-12 13:30:14 +0100

    introduce module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module corejava {
+        requires java.xml;
+        requires java.logging;
+        requires java.xml.crypto;
+        requires org.antlr.antlr4.runtime;
+        requires mockito.all;
+        requires org.jsoup;
+}
