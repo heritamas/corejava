bde02e5e335cac4ec16a61d34ae834f2195c36d5
Csenge Maruzsi <csenge.maruzsi@cloudera.com>
2022-01-21 13:40:09 +0100

    annotations

--- src/main/java/p2ch08/Annotations.java
+++ src/main/java/p2ch08/Annotations.java
@@ -11,5 +11,14 @@ public class Annotations {
         Annotated a = new Annotated();
 
         // invoke methods of a with arguments coming from annotations
+        System.out.println(a.methodA("Csovi"));
+        Class<? extends Annotated> clazz = a.getClass();
+        for (Method m : clazz.getDeclaredMethods()) {
+            Foo annotation = m.getAnnotation(Foo.class);
+            System.out.println("method name: " + m.getName());
+            if (annotation != null) {
+                System.out.println(a.methodA(annotation.object()));
+            }
+        }
     }
 }
--- src/main/java/p2ch08/Scripting.java
+++ src/main/java/p2ch08/Scripting.java
@@ -11,13 +11,14 @@ import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.Scanner;
 
 public class Scripting {
 
     static final ScriptEngineManager manager = new ScriptEngineManager();
 
-    public static void main(String[] args) throws URISyntaxException, IOException {
+    public static void main(String[] args) throws Exception {
         manager.getEngineFactories()
                 .stream()
                 .map(ScriptEngineFactory::getEngineName)
@@ -29,6 +30,15 @@ public class Scripting {
         String script = Files.readString(Paths.get(scriptUrl.toURI()));
 
         // execute script using variables coming from data.csv
+        List<String> variables = Files.readAllLines(Paths.get(Scripting.class.getResource("data.csv").toURI()));
+        Bindings binding = engine.createBindings();
+        for (String line : variables) {
+            engine.eval(line, binding);
+            engine.eval(script, binding);
+            System.out.printf("x1= %s, x2= %s\n", binding.get("x1"), binding.get("x2"));
+            binding.clear();
+        }
+        
 
     }
 }
